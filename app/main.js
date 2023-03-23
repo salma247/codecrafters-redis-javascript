dcdbeec9d5047b7f8953fe8177e9a9c52d2e27e2
salma247 <salmaelhamshary1@gmail.com>
2023-03-23 14:15:45 +0200

    PING

--- app/main.js
+++ app/main.js
@@ -6,6 +6,11 @@ console.log("Logs from your program will appear here!");
 
 const server = net.createServer((connection) => {
   // Handle connection
+  connection.on("data", (data) => {
+    connection.write("+PONG\r\n");
+  });
 });
+
+
 
 server.listen(6379, "127.0.0.1");
