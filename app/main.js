804bf5c8e9c80c4246c607d03a24eb8ca9af19b2
salma247 <salmaelhamshary1@gmail.com>
2023-03-23 14:31:54 +0200

    MULTIPING

--- app/main.js
+++ app/main.js
@@ -7,15 +7,9 @@ console.log("Logs from your program will appear here!");
 const server = net.createServer((connection) => { //create a server and pass a callback function to handle connection
   // Handle connection
   connection.on("data", (data) => { //connection is a socket , on data event means when data is received
-    //handle multiple ping pong
-    const message = data.toString(); //convert data to string
-    const messageArray = message.split("\n"); //split the string into an array
-    messageArray.forEach((message) => { //loop through the array
-        if (message === "ping") { //if the message is ping
-            connection.write("+PONG\r\n"); //write pong
-        }
+    connection.on("data", () => {
+        connection.write("+PONG\r\n");
     });
-
   });   
 });
 
