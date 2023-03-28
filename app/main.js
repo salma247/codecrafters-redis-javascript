bff3e67b8d53405ef19653e7521af5176a2c0581
salma247 <salmaelhamshary1@gmail.com>
2023-03-29 01:49:00 +0200

    concurrent clients

--- app/main.js
+++ app/main.js
@@ -6,6 +6,7 @@ console.log("Logs from your program will appear here!");
 
 const server = net.createServer((connection) => { //create a server and pass a callback function to handle connection
   // Handle connection
+  //because js supports event loop, we can serve multiple clients at the same time (asynchronous)
     connection.on("data", () => {
         connection.write("+PONG\r\n");
     });
@@ -13,4 +14,4 @@ const server = net.createServer((connection) => { //create a server and pass a c
 
 
 
-server.listen(6379, "127.0.0.1");
+server.listen(6379, "127.0.0.1"); //listen on port 6379
